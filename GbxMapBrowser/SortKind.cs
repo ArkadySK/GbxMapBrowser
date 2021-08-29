7ffb32e77bacdba83ef6b896beecdeef53e4e154
adam majerik <adamkooo2@gmail.com>
2021-08-29 19:05:37 +0200

    creation of sort class

---
+++ GbxMapBrowser/SortKind.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GbxMapBrowser
+{
+    public static class SortKind
+    {
+        public enum Kind
+        {
+            ByNameAscending,
+            ByNameDescending,
+            ByDateModifiedAscending,
+            ByDateModifiedDescending,
+            /*ByTitlepackAscending,
+            ByTitlepackDescending,*/
+            BySizeAscending,
+            BySizeDescending,
+            ByLendthAscending,
+            ByLendthDescending
+        }
+    }
+}
