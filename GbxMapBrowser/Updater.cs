effb66875c27a342701bd4523d78fafebfaa765d
adam majerik <adamkooo2@gmail.com>
2021-09-04 13:46:47 +0200

    added GitHubUpdate package, created updater class

---
+++ GbxMapBrowser/Updater.cs
@@ -0,0 +1,30 @@
+﻿using GitHubUpdate;
+using System;
+using System.Threading.Tasks;
+
+namespace GbxMapBrowser
+{
+    public static class Updater
+    {
+        public async static Task<bool> IsUpToDate()
+        {
+            var checker = new UpdateChecker("arkadySK", "GbxMapBrowser");
+
+            UpdateType update = await checker.CheckUpdate();
+            if (update == UpdateType.None) // up to date
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void DownloadUpdate()
+        {
+            var checker = new UpdateChecker("arkadySK", "GbxMapBrowser");
+            checker.DownloadAsset("GbxMapBrowser.zip"); // opens it in the user's browser
+        }
+    }
+}
