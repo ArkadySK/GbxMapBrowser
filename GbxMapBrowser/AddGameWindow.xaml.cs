01b0829f8434b5bfc6026a035b858926fea3d453
ArkadySK <adamkooo2@gmail.com>
2022-01-10 21:49:56 +0100

    created AddGameWindow

---
+++ GbxMapBrowser/AddGameWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GbxMapBrowser
+{
+    /// <summary>
+    /// Interaction logic for AddGameWindow.xaml
+    /// </summary>
+    public partial class AddGameWindow : Window
+    {
+        GbxGameController GameController;
+        GbxGame baseGbxGame;
+        public AddGameWindow(GbxGameController gbxGameController, GbxGame baseGame)
+        {
+            InitializeComponent();
+            nameTextBox.Text = baseGbxGame.Name + " New";
+            GameController = gbxGameController;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CustomGbxGame game = new CustomGbxGame(nameTextBox.Text, baseGbxGame);
+
+        }
+    }
+}
