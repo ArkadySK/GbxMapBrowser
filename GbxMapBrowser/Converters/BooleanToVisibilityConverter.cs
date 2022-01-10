6f74869fd79efe4f02f549bf7ffa2f5271a38376
ArkadySK <adamkooo2@gmail.com>
2022-01-10 20:30:15 +0100

    added bool converter

---
+++ GbxMapBrowser/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GbxMapBrowser.Converters
+{
+    internal class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not bool) 
+                return Visibility.Collapsed;
+
+            if((bool)value == true)
+                return Visibility.Visible;            
+            else 
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
