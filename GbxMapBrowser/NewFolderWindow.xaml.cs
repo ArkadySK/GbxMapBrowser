7964d1a9cbb957130495f59498648b76211ceee0
ArkadySK <adamkooo2@gmail.com>
2022-06-21 16:56:22 +0200

    added new folder window

---
+++ GbxMapBrowser/NewFolderWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GbxMapBrowser
+{
+    /// <summary>
+    /// Interaction logic for NewFolderWindow.xaml
+    /// </summary>
+    public partial class NewFolderWindow : Window
+    {
+        public string newName { get; set; }
+        public NewFolderWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveChanges()
+        {
+            if(nameTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter a name", "", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            newName = nameTextBox.Text;
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveChanges();
+        }
+
+        private void nameTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SaveChanges();
+            }
+        }
+    }
+}
