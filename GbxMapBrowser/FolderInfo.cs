ca31593778427cd0c2319adfe2ee7de17220c6e7
ArkadySK <adamkooo2@gmail.com>
2022-08-02 13:09:48 +0200

    added maps inside count

--- GbxMapBrowser/FolderInfo.cs
+++ GbxMapBrowser/FolderInfo.cs
@@ -8,8 +8,21 @@ namespace GbxMapBrowser
 {
     class FolderInfo: FolderAndFileInfo
     {
-        public int FilesInsideCount { get; private set; }
-        
+        public int FilesInsideCount
+        {
+            get
+            {
+                return GetFilesCount();
+            }
+        }
+        public int MapsInsideCount
+        {
+            get
+            {
+                return GetMapsCount();
+            }
+        }
+
 
         public FolderInfo(string fullnamepath)
         {
@@ -19,7 +32,6 @@ namespace GbxMapBrowser
             DateModified = directoryInfo.LastWriteTime;
             DateCreated = directoryInfo.CreationTime;
             ImageSmall = new Uri(Environment.CurrentDirectory + "\\Data\\UIIcons\\Folder.png");
-            FilesInsideCount = GetFilesCount();
         }
 
         int GetFilesCount()
@@ -30,6 +42,18 @@ namespace GbxMapBrowser
                 return Directory.GetFiles(FullPath).Length;
             }
             catch { return 0; }
+        }
+        int GetMapsCount()
+        {
+            if (!Directory.Exists(FullPath)) return 0;
+            try
+            {
+                var files = Directory.GetFiles(FullPath);
+                var maps = files.Where(x => x.EndsWith(".Gbx")).ToArray();
+                return maps.Length;
+            }
+            catch {}
+            return 0;
         }
     }
 }
